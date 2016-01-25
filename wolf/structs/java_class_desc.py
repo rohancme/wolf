638acb81db333146528c768a2f3cd4798c2b2aa1
Rohan Chakravarthy <rohanc219@gmail.com>
2016-01-25 13:17:54 -0500

    Added repr magic method for class desc

--- wolf/structs/java_class_desc.py
+++ wolf/structs/java_class_desc.py
@@ -5,3 +5,10 @@ class JavaClassDesc(object):
     def __init__(self, name, package):
         self.name = name
         self.package = package
+
+    def __str__(self):
+        string = "<Java Class Descriptor Object>"
+        string += "\nName: " + self.name
+        string += "\nPackage: " + self.package
+        string += "\n\t-------------------------"
+        return string
--- wolf/test.py
+++ wolf/test.py
@@ -1,10 +1,23 @@
 from repo_handler import RepoHandler
-
+from parser import Parser
 
 rh = RepoHandler("https://github.com/Netflix/Hystrix.git",
                  temp_dir_path='temp_dir')
 rh.clone_repo()
 for file_desc in rh.get_modified_files():
     print file_desc
+    print ""
+
+
+file_name_list = []
+
+for file_desc in rh.get_modified_files():
+    file_name_list += [file_desc.path]
+
+parser = Parser(file_name_list)
+
+for class_desc in parser.get_class_descs():
+    print class_desc
+    print ""
 
 rh.delete_temp_dir()
