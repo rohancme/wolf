7f04c3531b08cf6ce7599c2a3116c076a0c71919
Rohan Chakravarthy <rohanc219@gmail.com>
2016-04-12 15:40:55 -0400

    Install script for debian

---
+++ INSTALL.sh
@@ -0,0 +1,17 @@
+# Install python
+sudo apt-get install python -y
+sudo apt-get install python-dev -y
+
+# JAVA 8
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer -y
+
+# Maven3
+sudo apt-get install maven -y
+
+# Install libxml2
+sudo apt-get install libxml2 -y
+sudo apt-get install libxml2-dev -y
+sudo apt-get install libxslt1-dev -y
+sudo apt-get install zlib1g-dev -y
